f29b03004c66276b1ebec7f0eb867f217f8020ef
Kevin Salazar <137641381+KevinJSalazar@users.noreply.github.com>
2025-07-30 15:36:10 +0000

    Taller Completado

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,41 @@
 import './App.css'
 
-import Grid from '@mui/material/Grid2' 
+import Grid from '@mui/material/Grid2'
 import HydrologyTable from './components/HydrologyTable'
 import Student from './components/Student'
 
-// PENDIENTE: Cree la interfaz
 import DataHour from './interface/DataHour'
+import { useEffect, useState } from 'react'
 
 function App() {
 
   let url = "https://raw.githubusercontent.com/aavendan/datos/refs/heads/main/CELEC/hidrologia_17122024.json"
-  
-  // PENDIENTE: Variable de estado y la función de modificación. 
 
+  // Variable de estado y función de modificación
+  const [data, setData] = useState<DataHour[]>([])
 
-  // PENDIENTE: 
-  // Realizar una petición asíncrona a la URL. La respuesta es un JSON. 
-  // Al recibir la respuesta, actualice la variable de estado.
-
-  
-
+  // Petición asíncrona a la URL y actualización de la variable de estado
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error))
+  }, [url])
 
   return (
     <Grid container spacing={5}>
 
-        {/* Student */}
-        <Grid size={{ xs: 12 }}>
+      {/* Student */}
+      <Grid size={{ xs: 12 }}>
+        <Student apellidos="Salazar" nombres="Kevin" paralelo="1" />
+      </Grid>
 
-          {/* PENDIENTE: Envíe sus datos (apellidos, nombres y paralelo) como props del componente */}
-          <Student></Student>
+      {/* HydrologyTable */}
+      <Grid size={{ xs: 12 }}>
+        <HydrologyTable data={data}></HydrologyTable>
+      </Grid>
 
-        </Grid>
-        
-        {/* HydrologyTable */}
-        <Grid size={{ xs: 12 }}>
-
-          {/* PENDIENTE: Envíe la variable de estado como prop */}
-          <HydrologyTable data={ }></HydrologyTable>
-        
-        </Grid>
-        
-       
     </Grid>
   )
 }
-
 export default App
--- src/components/HydrologyTable.tsx
+++ src/components/HydrologyTable.tsx
@@ -26,10 +26,9 @@ export default function HydrologyTable( { data } : { data: DataHour[] } ) {
               {row.Fecha}
             </TableCell>
             <TableCell align="center">{row.Mazar}</TableCell>
-            
-            {/* PENDIENTE: Valores a renderizar en cada celda  */}
-            
-            
+            <TableCell align="center">{row.Molino}</TableCell>
+            <TableCell align="center">{row.Sopladora}</TableCell>
+
           </TableRow>
         ))
       )
@@ -51,10 +50,8 @@ export default function HydrologyTable( { data } : { data: DataHour[] } ) {
           <TableRow>
             <TableCell>Fecha</TableCell>
             <TableCell align='center'>Mazar</TableCell>
-            
-            {/* PENDIENTE: Cabeceras de las columnas  */}
-            
-
+            <TableCell align='center'>Molino</TableCell>
+            <TableCell align='center'>Sopladora</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
